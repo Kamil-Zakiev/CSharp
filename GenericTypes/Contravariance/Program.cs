350364585278a72e53094cb0fd0626b3171528c5
Камиль Закиев <k.zakiev@mail.ru>
2017-07-29 13:39:56 +0300

    учебные проекты по GenericTypes

---
+++ GenericTypes/Contravariance/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Contravariance
+{
+    internal interface IGenericType<in T> { }
+
+    public class GenericClass<T> : IGenericType<T> { }
+
+    public class A { }
+
+    public class B : A { }
+
+
+    public sealed class Program
+    {
+        private static void Method(IGenericType<B> gen)
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            ContravarianceOnClass();
+            ContravarianceOnInterface();
+        }
+
+        private static void ContravarianceOnClass()
+        {
+            var a = new GenericClass<A>();
+            var b = new GenericClass<B>();
+            // Error CS0029  Cannot implicitly convert type 'Contravariance.GenericClass<Contravariance.A>' to 'Contravariance.GenericClass<Contravariance.B>'
+            // b = a;
+        }
+
+
+        private static void ContravarianceOnInterface()
+        {
+            IGenericType<A> a = new GenericClass<A>();
+            IGenericType<B> b = new GenericClass<B>();
+            b = a;
+
+            // контрвариантность позволяет передать объект класса, реализующий IGenericType<A>, в метод, ожидающий объект,  реализующий IGenericType<B>
+            // на интерфейсах это пиздецки сложно догнать, на делегатах - очевидное поведение
+            Method(a);
+        }
+    }
+}
---
+++ GenericTypes/TypeCtor/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace TypeCtor
+{
+    using System;
+
+    public class GenericClass<T>
+    {
+        private T _field;
+
+        static GenericClass()
+        {
+            Console.WriteLine("GenericStaticCtor: Class was closed with type " + typeof(T).FullName);
+        }
+    }
+
+    public class A : GenericClass<A>
+    {
+        static A()
+        {
+            Console.WriteLine("static A ctor");
+        }
+
+        public A()
+        {
+            Console.WriteLine("instance .ctor A");
+        }
+
+        public static void Method()
+        {
+            Console.WriteLine("static method in class A");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // стат. конструктор выполняется сразу после использования типа: создание экземплярв типа, вызов стат методов
+            InstanceObject();
+            StaticMethodCall();
+            TypeMetaUsing();
+        }
+
+        private static void InstanceObject()
+        {
+            Console.WriteLine("InstanceObject:");
+            var a = new A();
+            // static A ctor
+            // GenericStaticCtor: Class was closed with type TypeCtor.A
+            // instance.ctor A
+
+            Console.WriteLine();
+        }
+
+        private static void StaticMethodCall()
+        {
+            Console.WriteLine("StaticMethodCall:");
+            A.Method();
+            // static A ctor
+            // static method in class A
+
+            Console.WriteLine();
+        }
+
+        private static void TypeMetaUsing()
+        {
+            Console.WriteLine("TypeMetaUsing:");
+            Console.WriteLine(typeof(A));
+            // TypeCtor.A
+
+            Console.WriteLine();
+        }
+    }
+}
