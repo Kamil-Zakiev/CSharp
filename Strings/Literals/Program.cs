a0584d53d95fecaf75318f3d5ed707c6e4533420
Камиль Закиев <k.zakiev@mail.ru>
2017-08-01 12:28:30 +0300

    strings

---
+++ Strings/Literals/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Literals
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Example1();
+            Example2();
+            Example3();
+        }
+
+        /// <summary>
+        ///     Компилятор помещает строку "asd" в метаданные и далее использует ссылку на этот объект.
+        /// </summary>
+        private static void Example1()
+        {
+            var s1 = "asd";
+            var s2 = "asd";
+
+            Console.WriteLine(ReferenceEquals(s1, s2));
+            // output: true
+        }
+
+        /// <summary>
+        ///     При обычном чтении строк из файла интернирования не происходит, даже без указания атрибутов
+        /// </summary>
+        private static void Example2()
+        {
+            var lines = File.ReadAllLines(@"G:\CSharp\Strings\Literals\TextFile.txt");
+            var firstLine = lines.First();
+            var words = firstLine.Split(' ');
+
+            var s1 = words[0];
+            var s2 = words[1];
+            Console.WriteLine("\"" + s1 + "\"");
+            Console.WriteLine("\"" + s2 + "\"");
+            Console.WriteLine(ReferenceEquals(s1, s2));
+            // output: false
+        }
+
+
+        /// <summary>
+        ///     Явное интернирование
+        /// </summary>
+        private static void Example3()
+        {
+            var lines = File.ReadAllLines(@"G:\CSharp\Strings\Literals\TextFile.txt");
+            var firstLine = lines.First();
+            var words = firstLine.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = String.Intern(words[i]);
+            }
+
+            var s1 = words[0];
+            var s2 = words[1];
+            Console.WriteLine("\"" + s1 + "\"");
+            Console.WriteLine("\"" + s2 + "\"");
+            Console.WriteLine(ReferenceEquals(s1, s2));
+            // output: true
+        }
+    }
+}
---
+++ Strings/LiteralsOnDotNet4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Literals
+{
+    using System;
+    using System.Runtime.CompilerServices;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Example();
+        }
+
+        /// <summary>
+        ///     Компилятор помещает строку "asd" в метаданные и далее использует ссылку. Происходит интернирование, хоть я и
+        ///     повесил атрибут и на сборку, и на метод
+        /// </summary>
+        [CompilationRelaxations(CompilationRelaxations.NoStringInterning)]
+        private static void Example()
+        {
+            string s1 = "asd";
+            string s2 = "asd";
+
+            Console.WriteLine(ReferenceEquals(s1, s2));
+            // output: true
+        }
+    }
+}
