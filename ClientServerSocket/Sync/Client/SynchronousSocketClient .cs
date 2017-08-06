c90023d34496023a1ec5d9e5b31c13696fd03b6f
Камиль Закиев <k.zakiev@mail.ru>
2017-08-06 07:23:31 +0300

    сокет, синхронный клиент-сервер

---
+++ ClientServerSocket/Sync/Client/SynchronousSocketClient .cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class SynchronousSocketClient
+    {
+        public static void StartClient()
+        {
+            byte[] buffer;
+
+            try
+            {
+                // Установим удаленную точке для сокета
+                // Этот пример используется порт с номером 11000 локального компьютера
+                var hostName = Dns.GetHostName();
+                var ipHostInfo = Dns.Resolve(hostName);
+                var ipAddress = ipHostInfo.AddressList[0];
+                var remoteEndPoint = new IPEndPoint(ipAddress, 11000);
+
+                // Создание сокета TCP/IP
+                var senderSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Подключаем сокет к удаленной точке
+                senderSocket.Connect(remoteEndPoint);
+
+                Console.WriteLine();
+                Console.WriteLine("Сокет выполнил соединение с  {0}", senderSocket.RemoteEndPoint);
+
+                var userLine = Console.ReadLine();
+                var message = userLine + Environment.NewLine +
+                              $"Время на текущий момент: {DateTime.Now.ToLocalTime()}";
+                var bytesToSend = Encoding.UTF8.GetBytes(message);
+                senderSocket.Send(bytesToSend);
+
+                // Получаем ответ от удаленного устройства
+                buffer = new byte[128];
+                var bytesCount = senderSocket.Receive(buffer);
+                var responseMessage = Encoding.UTF8.GetString(buffer, 0, bytesCount);
+                Console.WriteLine("Удаленное устройство ответило следующее: {0}", responseMessage);
+
+                // Закрываем сокет
+                senderSocket.Shutdown(SocketShutdown.Both);
+                senderSocket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                StartClient();
+            }
+        }
+    }
+}
---
+++ ClientServerSocket/Sync/Server/SynchronousSocketListener .cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    internal class SynchronousSocketListener
+    {
+        // Входящие данные от клиента.
+        public static string data;
+
+        private static int bufferSize = 32;
+
+        public static void StartListening()
+        {
+            // буфер для входных данных
+            byte[] buffer;
+
+            // Установим локальную конечную точку сокета
+            // Dns.GetHostName возвращает имя хоста, выполняющего приложение
+            var hostName = Dns.GetHostName();
+            var ipHostInfo = Dns.Resolve(hostName);
+            var ipAddress = ipHostInfo.AddressList[0]; // 192.168.1.106
+            var localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            // Создание сокета TCP/IP
+            var listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // Связывание сокета с локальной конечной точкой
+                listenerSocket.Bind(localEndPoint);
+
+                // Прослушивание входящих соединений, указываем максимальное количество запросов в очереди обработки
+                listenerSocket.Listen(10);
+
+                // Начало прослуживание соединений
+                while (true)
+                {
+                    Console.WriteLine("Ожидание подключения . . . ");
+
+                    // Программа останавливается в ожидании входящего соединения
+                    // При получении соединения создается новый сокет для обработки соединения
+                    var handlerSocket = listenerSocket.Accept();
+                    
+                    // Обработка входящего соединения
+                    var recievedBytes = new List<byte>();
+                    while (true)
+                    {
+                        buffer = new byte[bufferSize];
+
+                        // заполняем буфер, возвращается количество записанных байтов
+                        var bytesRec = handlerSocket.Receive(buffer);
+                        recievedBytes.AddRange(buffer.Take(bytesRec));
+
+                        // Если получение магическое окончание слова, то прекращаем считывание
+                        if (bytesRec < bufferSize)
+                            break;
+                    }
+                    data = Encoding.UTF8.GetString(recievedBytes.ToArray(), 0, recievedBytes.Count);
+
+                    var message = $"Получено сообщение: {data}";
+                    Console.WriteLine(message);
+                    Thread.Sleep(3000);
+
+                    // отвечаем клиенту
+                    var msg = Encoding.UTF8.GetBytes(data);
+                    handlerSocket.Send(msg);
+                    handlerSocket.Shutdown(SocketShutdown.Both);
+                    handlerSocket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            StartListening();
+        }
+    }
+}
