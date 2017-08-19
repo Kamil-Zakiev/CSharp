c429532ae613d9b44002e4585d947b6c96c91ce9
Камиль Закиев <k.zakiev@mail.ru>
2017-08-19 13:33:07 +0300

    типы значения, проверка скорости при реализации хеша: 3мс против 372мс

---
+++ ReferenceTypeVsValueType/ValueTypeHashCode/ListExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValueTypeHashCode
+{
+    public static class ListExtensions
+    {
+        private static Random _random = new Random();
+
+        private static void Swap<T>(List<T> list, int i1, int i2)
+        {
+            T c = list[i1];
+            list[i1] = list[i2];
+            list[i2] = c;
+        }
+
+        public static void Shuffle<T>(this List<T> list)
+        {
+            for (var index = 0; index < list.Count; index++)
+            {
+                var i1 = _random.Next() % list.Count;
+                var i2 = _random.Next() % list.Count;
+
+                Swap(list, i1, i2);
+            }
+        }
+    }
+}
---
+++ ReferenceTypeVsValueType/ValueTypeHashCode/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ValueTypeHashCode
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var listOfKeys = new List<KeyStruct>();
+
+            var dict1 = GetDict(listOfKeys);
+
+            listOfKeys.Shuffle();
+
+            var totalSum = 0m;
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            foreach (var key in listOfKeys)
+            {
+                var value = dict1[key];
+                totalSum += value;
+            }
+            stopWatch.Stop();
+            Console.WriteLine("Total sum = " + totalSum);
+            Console.WriteLine("Elapsed ms = " + stopWatch.ElapsedMilliseconds);
+        }
+
+        private static Dictionary<KeyStruct, decimal> GetDict(List<KeyStruct> listOfKeys)
+        {
+            var dict1 = new Dictionary<KeyStruct, decimal>();
+            for (var i = 0; i < 1000; i++)
+            {
+                var key = new KeyStruct
+                {
+                    Guid1 = Guid.NewGuid(),
+                    Guid2 = Guid.NewGuid(),
+                    Guid3 = Guid.NewGuid(),
+                    Guid4 = Guid.NewGuid()
+                };
+                listOfKeys.Add(key);
+                dict1.Add(key, (decimal) i * i);
+            }
+
+            return dict1;
+        }
+    }
+
+    public struct KeyStruct
+    {
+        public Guid? Guid1;
+        public Guid? Guid2;
+        public Guid? Guid3;
+        public Guid? Guid4;
+
+        public override int GetHashCode()
+        {
+            return Guid1.GetHashCode() ^ (Guid2.GetHashCode() ^ 13) ^ (Guid3.GetHashCode() ^ 26) ^ (32 ^ Guid4.GetHashCode());
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            var isTheSameType = obj is KeyStruct?;
+            if (!isTheSameType)
+            {
+                return false;
+            }
+            var value = (KeyStruct)obj;
+
+            if (value.Guid1 == Guid1 && value.Guid2 == Guid2 && value.Guid3 == Guid3 && value.Guid4 == Guid4)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
