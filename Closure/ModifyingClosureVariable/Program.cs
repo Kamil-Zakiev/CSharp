bd7f3035a541c6c5b0f4fa58e00cbf0926d41289
Камиль Закиев <k.zakiev@mail.ru>
2017-08-20 08:11:38 +0300

    изменение переменной из замыкания

---
+++ Closure/ModifyingClosureVariable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ModifyingClosureVariable
+{
+    internal class Program
+    {
+        public static int b = 123;
+        private static void Main(string[] args)
+        {
+            var a = 1; // temp.a = 1
+            Action lamba = () =>
+            {
+                Console.WriteLine(a);
+                // Console.WriteLine(temp.a);
+            };
+            a = 2; // temp.a = 2
+            lamba();
+            // output: 2
+        }
+    }
+}
