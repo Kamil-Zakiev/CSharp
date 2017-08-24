053f272219eb52ab601bf2d67f89e1cffa0df4cb
Камиль Закиев <k.zakiev@mail.ru>
2017-08-24 07:24:47 +0300

    многопоточность - стат. конструктор

---
+++ Multithreating/StaticCtorAsConcurrent/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace StaticCtorAsConcurrent
+{
+    public class MyClass
+    {
+        static MyClass()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name);
+            Console.WriteLine("MyClass static ctor");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Method()
+        {
+            var a = new MyClass();
+            Console.WriteLine(a);
+        }
+
+        /// <summary> Статический конструктор выполняется один раз, не получилось вызвать его дважды </summary>
+        private static void Main(string[] args)
+        {
+            var thread1 = new Thread(Method);
+            var thread2 = new Thread(Method);
+
+            thread1.Name = "Thread1";
+            thread2.Name = "Thread2";
+
+            thread2.Start();
+            thread1.Start();
+        }
+    }
+}
