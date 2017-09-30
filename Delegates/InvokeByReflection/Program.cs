abce5d0248bd4e98465d4bd8c8cdecf4f1530897
Камиль Закиев <k.zakiev@mail.ru>
2017-09-30 09:39:13 +0300

    делегаты: лямбда-выражения, замыкание, создание делегата с помощью отражения

---
+++ Delegates/InvokeByReflection/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+
+namespace InvokeByReflection
+{
+    internal delegate void MyDelegate(int n);
+
+    internal struct Program
+    {
+        /// <summary>
+        ///     При помощи Delegate.DynamicInvoke можно вызвать обратный метод,
+        ///     но если аргумент - экз. значимого типа - будет упаковка
+        /// </summary>
+        private static void Example1()
+        {
+            Action<int> action = n => Console.WriteLine(n);
+            Delegate del = action;
+            del.DynamicInvoke();
+            // public object DynamicInvoke(params object[] args)
+        }
+
+        private static void Method(int n)
+        {
+            Console.WriteLine(n);
+        }
+
+        /// <summary>
+        ///     Создание экземпляра делегата - обертки для стат. функции - и последующий вызов с помощью механизма отражения
+        /// </summary>
+        private static void Example2()
+        {
+            var delType = Type.GetType("InvokeByReflection.MyDelegate");
+            var mi = typeof(Program).GetTypeInfo().GetDeclaredMethod("Method");
+
+            // создание обертки-делегата для функции
+            var deleg = mi.CreateDelegate(delType);
+            deleg.DynamicInvoke(64);
+        }
+
+
+        private static void Main(string[] args)
+        {
+            Example2();
+        }
+    }
+}
---
+++ Delegates/Lambdas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Lambdas
+{
+    /// <summary>
+    /// см. ILDasm
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // ReSharper disable once ConvertClosureToMethodGroup
+            int a = Console.Read();
+            MyMethod(n => Console.WriteLine(a));
+        }
+
+        private static void MyMethod(WaitCallback actionRecievingInt)
+        {
+            actionRecievingInt(5);
+        }
+    }
+}
