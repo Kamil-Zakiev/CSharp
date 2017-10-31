8054e59c6578bae8c0258331a469f285e757b61a
Камиль Закиев <k.zakiev@mail.ru>
2017-10-31 08:10:23 +0300

    GCHandle: попробовал отобразить изменение значения указателя объекта - не получилось

---
+++ GCExamples/GCExamples/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GCExamples
+{
+    internal class Program
+    {
+         void M1()
+        {
+            
+        }
+
+        static GCHandle M()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var q = new Program();
+                q.M1();
+            }
+
+            var gcHandle = GCHandle.Alloc(new int(), GCHandleType.Normal);
+
+            Console.WriteLine("Target:\t\t" + gcHandle.Target);
+            Console.WriteLine((IntPtr)gcHandle);
+            Console.WriteLine("IsAllocated:\t" + gcHandle.IsAllocated);
+
+            return gcHandle;
+        }
+
+        public static unsafe   void ghjk()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var q = new Program();
+            }
+
+            IntPtr origin;
+            var bytes = new byte[1999];
+            
+            fixed (Byte* pbytes = bytes)
+            {
+                origin = (IntPtr) pbytes;
+            }
+
+            GC.Collect();
+
+
+            IntPtr second;
+            fixed (Byte* pbytes = bytes)
+            {
+                second = (IntPtr)pbytes;
+                Console.WriteLine(second == origin);
+                // true
+            }
+
+        }
+
+        private static void Main()
+        {
+            ghjk();
+            return;
+            
+            var gcHandle = M();
+            GC.Collect();
+            GC.WaitForFullGCComplete();
+
+            Console.WriteLine("Target:\t\t" + gcHandle.Target);
+            Console.WriteLine((IntPtr)gcHandle);
+            Console.WriteLine("IsAllocated:\t" + gcHandle.IsAllocated);
+        }
+    }
+}
