60900057a2cd7c45aabf81a9e019a81ed290d830
Камиль Закиев <k.zakiev@mail.ru>
2018-01-01 12:14:02 +0300

    Пример использования задачи-продолжения

---
+++ Multithreating/TaskContinuasion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskContinuasion
+{
+    internal class Program
+    {
+        /// <summary> Пример использования задачи-продолжения </summary>
+        private static void Main()
+        {
+            var task = new Task<string>(() =>
+            {
+                Thread.Sleep(500); 
+                // throw new Exception();
+                return "----->  Result of task  <---";
+            });
+
+            task.ContinueWith(t => Console.WriteLine(t.Result), TaskContinuationOptions.OnlyOnRanToCompletion);
+            task.ContinueWith(t => Console.WriteLine("Ошибка произошла."), TaskContinuationOptions.OnlyOnFaulted);
+
+            task.Start();
+            Thread.Sleep(1000); // потому что задание - это частный случай использования потока из пула
+        }
+    }
+}
