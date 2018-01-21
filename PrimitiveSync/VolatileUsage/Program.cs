1f2269a473bdfba9e607d3ff5f8cd947361ceeb4
Камиль Закиев <k.zakiev@mail.ru>
2018-01-21 22:31:44 +0300

    volatile usage

---
+++ PrimitiveSync/VolatileUsage/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace VolatileUsage
+{
+    internal class Program
+    {
+        private static bool s_stopWorker;
+
+        /// <summary>
+        ///     Пример 1: при релизной версии кода код ведет себя некорректно в многопоточной среде из-за оптимизации
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Main: letting worker run for 2 seconds");
+            new Thread(Worker).Start();
+            Thread.Sleep(2000);
+            s_stopWorker = true;
+            Console.WriteLine("Main: waiting for worker to stop");
+        }
+
+        private static void Worker()
+        {
+            var x = 0;
+            while (!Volatile.Read(ref s_stopWorker))
+                // while (!s_stopWorker) // <- при релизной версии кода будет зацикливание: 
+                //    считывание при каждой итерации происходить не будет
+            {
+                x++;
+            }
+
+            Console.WriteLine("Worker: stopped when x={0}", x);
+        }
+    }
+}
---
+++ PrimitiveSync/VolatileUsage2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace VolatileUsage2
+{
+    internal sealed class ThreadsSharingData
+    {
+        private int m_flag = 0;
+        private int m_value = 0;
+
+        public void Thread1()
+        {
+            m_value = 5;
+            m_flag = 1;
+        }
+
+        public void Thread2()
+        {
+            if (m_flag == 1)
+            {
+                Console.WriteLine(m_value);
+            }
+        }
+    }
+
+    internal class Program
+    {
+        /// <summary>
+        ///     Здесь теоретически возможны косяки с {переключением между потоками}/{паралелльным выполнением потоков}
+        ///     Проблемы решаются волатильным чтением/записью в конце/в начале инструкций
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            var tsd = new ThreadsSharingData();
+            new Thread(tsd.Thread2).Start();
+            new Thread(tsd.Thread1).Start();
+        }
+    }
+}
