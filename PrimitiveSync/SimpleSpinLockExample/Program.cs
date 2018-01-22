a20288ebf81822744e093f888f184c1c309c74bb
Камиль Закиев <k.zakiev@mail.ru>
2018-01-22 21:22:50 +0300

    simple spin lock example

---
+++ PrimitiveSync/SimpleSpinLockExample/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleSpinLockExample
+{
+    internal class Program
+    {
+        private static SimpleSpinLock mSimpleSpinLock;
+        
+        private static bool _flag = true;
+
+        private static int _x;
+
+        private static void Locked(Action action)
+        {
+            mSimpleSpinLock.Enter();
+            action();
+            mSimpleSpinLock.Leave();
+        }
+
+        private static void Method()
+        {
+            // цикл, чтобы позже запустить потоки практически одновременно
+            while (_flag)
+            {
+            }
+
+            Locked(() =>
+            {
+                // Тут могут быть громоздкие вычисления
+                // Доступ к ресурсу в каждый момент времени имеет только один поток...
+                _x++;
+                Thread.Sleep(1);
+            });
+        }
+
+        private static void Main()
+        {
+            const int count = 6;
+            var tasks = new Task[count];
+            for (var i = 0; i < count; i++)
+            {
+                tasks[i] = new Task(Method);
+                tasks[i].Start();
+            }
+            
+            _flag = false;
+
+            Task.WaitAll(tasks);
+
+            Console.WriteLine("Expected Value:\t" + count);
+            Console.WriteLine("Fact Value:\t" + _x);
+            Console.WriteLine("\nIdle Count: \t" + mSimpleSpinLock.idleCount);
+        }
+    }
+}
---
+++ PrimitiveSync/SimpleSpinLockExample/SimpleSpinLock.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+
+namespace SimpleSpinLockExample
+{
+    internal struct SimpleSpinLock
+    {
+        private int m_resourceIsInUse;
+
+        public int idleCount;
+
+        public void Enter()
+        {
+            while (true)
+            {
+                var prevValue = Interlocked.Exchange(ref m_resourceIsInUse, 1);
+                if (prevValue == 0)
+                {
+                    return;
+                }
+
+                // тут можно схитрожопить и задержать поток
+                // Thread.Yield(); // <- попросить Windows запланировать для текущего процессора другой поток на след. такт
+                // Thread.Sleep(0); // <- заствляет запланировать другой поток для исполнения ( потокам с низким приоритетом не дают исполняться)
+                // Thread.Sleep(1); // <- включает принудительное переключение контекста
+                // Thread.SpinWait(50); // <- про гиперпотоковые процессоры... (используется в SpinLock из FCL)
+                Interlocked.Increment(ref idleCount);
+            }
+        }
+
+        public void Leave()
+        {
+            // тут можно и просто " = 0", тем более для Int32 гарантирована атомарная запись
+            // но, видимо, каждый дрочит как хочет
+            // Volatile.Write(ref m_resourceIsInUse, 0);
+            m_resourceIsInUse = 0;
+        }
+    }
+}
