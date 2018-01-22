0a0f1fc5f5a9d3f00bb1f93aff2a263e0595a48c
Камиль Закиев <k.zakiev@bars-open.ru>
2018-01-22 14:17:47 +0300

    поправил примеры по загрузке сборок

--- AssembliesLoading/AssembliesLoading/Program.cs
+++ AssembliesLoading/AssembliesLoading/Program.cs
@@ -9,7 +9,7 @@ namespace AssembliesLoading
 {
     internal class Program
     {
-        static List<Type> types = new List<Type>();
+        private static List<Type> types = new List<Type>();
 
         private static void Main(string[] args)
         {
@@ -20,6 +20,8 @@ namespace AssembliesLoading
         ///     Сборка загружается в домен при генерации машинного кода JIT-компилятором
         ///     Использование класса = создание экземпляра или вызов стат. метода
         ///     а также использование типа
+        ///     Примечание. При релизной версии кода сборка подгрузилась сразу будущем создании экземпляра класса и вызове
+        ///     стат.метода
         /// </summary>
         private static void Example1()
         {
@@ -39,15 +41,13 @@ namespace AssembliesLoading
 
             Console.WriteLine("Exported types:");
             foreach (var assemExportedType in assem.ExportedTypes)
-            {
                 Console.WriteLine(assemExportedType.FullName);
-            }
         }
 
         /// <summary> Создание экземпляра типа, определенного в загруженной сборке </summary>
         private static void Example3()
         {
-           // var assem = Assembly.Load("SomeAssembly, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+            // var assem = Assembly.Load("SomeAssembly, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
             PrintAssemblesInCurrentAppDomain();
 
             // не дает загружать типы из другой сборки, хоть сборка и в домене
@@ -58,57 +58,56 @@ namespace AssembliesLoading
 
             // var someType = assem.ExportedTypes.First(t => t.Name == "SomeClass");
 
-            object someInstance1 = System.Activator.CreateInstance(someType, 1, 2, 3);
+            var someInstance1 = Activator.CreateInstance(someType, 1, 2, 3);
 
             // десериализованный объект ?
-            object someInstance2 = System.Activator.CreateInstance("SomeAssembly, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", 
-                "SomeAssembly.SomeClass")
+            var someInstance2 = Activator.CreateInstance(
+                    "SomeAssembly, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+                    "SomeAssembly.SomeClass")
                 .Unwrap();
             // десериализованный объект - хотя мб хендлер указывает на объект в текущем домене
             var isProxy = RemotingServices.IsTransparentProxy(someInstance2);
 
-            object someInstance3 = AppDomain.CurrentDomain.CreateInstanceAndUnwrap(
+            var someInstance3 = AppDomain.CurrentDomain.CreateInstanceAndUnwrap(
                 "SomeAssembly, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
                 "SomeAssembly.SomeClass");
             isProxy = RemotingServices.IsTransparentProxy(someInstance3);
 
-            object someInstance4 = someType.GetTypeInfo()
+            var someInstance4 = someType.GetTypeInfo()
                 .DeclaredConstructors
                 .First()
                 .Invoke(null);
 
             // создание экземпляра обощенного типа
-            object o5 = Activator.CreateInstance(typeof(List<>).MakeGenericType(typeof(int)));
+            var o5 = Activator.CreateInstance(typeof(List<>).MakeGenericType(typeof(int)));
         }
 
         /// <summary> Вызов метода </summary>
         private static void Example4()
         {
-
         }
 
         /// <summary> Вызов метода, предлагаемый Рихтером </summary>
         private static void Example5()
         {
-
         }
-
 
 
         private static void Method()
         {
             types.Add(typeof(SomeClass));
 
-            //Console.WriteLine("Методы класса SomeClass:" + typeof(SomeClass).GetMethods().Select(x => x.Name).Aggregate((n1,n2) => n1 += "; "  + n2));
+            // Console.WriteLine("Методы класса SomeClass:" + typeof(SomeClass).GetMethods().Select(x => x.Name).Aggregate((n1,n2) => n1 += "; "  + n2));
 
             //var t = new SomeClass();
 
-            // SomeClass.M();
+            //SomeClass.M();
         }
 
         private static void PrintAssemblesInCurrentAppDomain()
         {
             var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            Console.WriteLine("Сборок в текущем домене: " + assemblies.Length);
             Console.WriteLine(string.Join(Environment.NewLine, assemblies.Select(assembly => assembly.FullName)));
         }
     }
