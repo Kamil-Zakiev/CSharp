44e1c88d5d05241bf47b044ff8b416f3c6268264
Камиль Закиев <k.zakiev@bars-open.ru>
2018-01-22 17:01:13 +0300

    my threads shared data access example

---
+++ PrimitiveSync/MultiThreadSharedDataTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadSharedDataTest
+{
+    internal class Program
+    {
+        private static bool _flag = true;
+        private static int _x;
+
+        private static void Method()
+        {
+            while (_flag)
+            {
+            }
+
+            Interlocked.Increment(ref _x);
+            //_x++;
+        }
+
+        public static void Main(string[] args)
+        {
+            const int count = 6;
+            for (var i = 0; i < count; i++)
+                new Thread(Method).Start();
+
+            Thread.Sleep(200);
+            _flag = false;
+            Console.WriteLine("Expected Value:\t" + count);
+            Console.WriteLine("Fact Value:\t" + _x);
+        }
+    }
+}
