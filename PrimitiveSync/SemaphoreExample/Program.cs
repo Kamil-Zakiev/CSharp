1a98e458a13cd219a109f64b9858e3f6346a580d
Камиль Закиев <k.zakiev@mail.ru>
2018-01-24 20:42:19 +0300

    Semaphore Example

---
+++ PrimitiveSync/SemaphoreExample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreExample
+{
+
+    internal class Program
+    {
+        private static readonly Semaphore Semaphore = new Semaphore(0, 600);
+
+        private static readonly EventWaitHandle AutoResetEvent = new AutoResetEvent(true);
+
+        private static int _x;
+
+        private static void Method()
+        {
+            // в будущем значение семафора станет 600 и все потоки пула (их будет штук 4-12) запустятся
+            Semaphore.WaitOne();
+
+            AutoResetEvent.WaitOne();
+            _x++;
+            AutoResetEvent.Set();
+        }
+
+        public static void Main(string[] args)
+        {
+            const int count = 600;
+            var tasks = new Task[count];
+            for (var i = 0; i < count; i++)
+            {
+                tasks[i] = new Task(Method);
+                tasks[i].Start();
+            }
+
+            Semaphore.Release(count);
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            Task.WaitAll(tasks);
+
+            Console.WriteLine("Elapsed ticks: " + stopWatch.ElapsedTicks); // 7k - 12k
+
+            Console.WriteLine("Expected Value:\t" + count);
+            Console.WriteLine("Fact Value:\t" + _x);
+        }
+    }
+}
