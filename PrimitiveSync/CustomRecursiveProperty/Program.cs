08e7979665cacd2b4344abd8cd62346967bd71d4
Камиль Закиев <k.zakiev@mail.ru>
2018-01-25 21:48:09 +0300

    without mutex: CustomRecursiveProperty

---
+++ PrimitiveSync/CustomRecursiveProperty/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace CustomRecursiveProperty
+{
+    internal class Program
+    {
+        private static void Method1(Action enter, Action leave)
+        {
+            enter();
+            Method2(enter, leave);
+            leave();
+        }
+
+        private static void Method2(Action enter, Action leave)
+        {
+            enter();
+            // some work
+            leave();
+        }
+
+        private static void Main(string[] args)
+        {
+            var mutex = new RecursiveAutoResetEvent();
+            Action enter2 = () => mutex.Enter();
+            Action leave2 = () => mutex.Leave();
+            var thread = new Thread(() => Method1(enter2, leave2));
+            thread.Start();
+            thread.Join();
+            Console.WriteLine("ex1");
+            // works fine
+        }
+    }
+}
---
+++ PrimitiveSync/CustomRecursiveProperty/RecursiveAutoResetEvent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace CustomRecursiveProperty
+{
+    internal class RecursiveAutoResetEvent : IDisposable
+    {
+        private readonly AutoResetEvent _autoResetEvent = new AutoResetEvent(true);
+        private int _owningThreadId;
+        private int _recursionCount;
+
+        public void Dispose()
+        {
+            _autoResetEvent.Dispose();
+        }
+
+        public void Enter()
+        {
+            var currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            if (_owningThreadId == currentThreadId)
+            {
+                _recursionCount++;
+                return;
+            }
+
+            _autoResetEvent.WaitOne();
+
+            _recursionCount = 1;
+            _owningThreadId = currentThreadId;
+        }
+
+        public void Leave()
+        {
+            var currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            if (currentThreadId != _owningThreadId)
+            {
+                throw new InvalidOperationException();
+            }
+
+            _recursionCount--;
+            if (_recursionCount == 0)
+            {
+                _owningThreadId = 0;
+                _autoResetEvent.Set();
+            }
+        }
+    }
+}
