e7a9498b727901558ecb57bd4340f5f6f3f59d96
Камиль Закиев <k.zakiev@mail.ru>
2018-01-27 11:13:22 +0300

    simple hybrid lock

---
+++ HybridConstructions/SimpleHybridLock/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace SimpleHybridLock
+{
+    internal static class Logger
+    {
+        private static ConcurrentDictionary<int, ConsoleColor> colorDict =
+            new ConcurrentDictionary<int, ConsoleColor>();
+
+        private static ConcurrentQueue<ConsoleColor> stack = new ConcurrentQueue<ConsoleColor>();
+
+        static Logger()
+        {
+            stack.Enqueue(ConsoleColor.Blue);
+            stack.Enqueue(ConsoleColor.Green);
+            stack.Enqueue(ConsoleColor.DarkYellow);
+            stack.Enqueue(ConsoleColor.DarkBlue);
+            stack.Enqueue(ConsoleColor.DarkMagenta);
+            stack.Enqueue(ConsoleColor.Red);
+            stack.Enqueue(ConsoleColor.DarkCyan);
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        private static readonly AutoResetEvent AutoResetEvent = new AutoResetEvent(true);
+
+        public static void Log(int threadId, string msg)
+        {
+            AutoResetEvent.WaitOne();
+            ConsoleColor consoleColor;
+            if (!colorDict.ContainsKey(threadId) && stack.TryDequeue(out consoleColor))
+            {
+                stack.Enqueue(consoleColor);
+                colorDict[threadId] = consoleColor;
+            }
+
+            Console.BackgroundColor = colorDict[threadId];
+            Console.WriteLine($"Thread #{threadId}: {msg}");
+
+            AutoResetEvent.Set();
+        }
+    }
+
+    internal class Program
+    {
+        private static readonly SimpleHybridLock SimpleHybridLock = new SimpleHybridLock();
+
+        private static readonly ManualResetEvent ManualResetEvent = new ManualResetEvent(false);
+
+        private static void Thread1()
+        {
+            ManualResetEvent.WaitOne();
+
+            SimpleHybridLock.Enter();
+            // some work
+            SimpleHybridLock.Leave();
+        }
+
+        /// <summary>Конкуренция потоков </summary>
+        private static void Test1()
+        {
+            Console.WriteLine("Threads contest situation:");
+            ManualResetEvent.Reset();
+
+            new Thread(Thread1).Start();
+            new Thread(Thread1).Start();
+            new Thread(Thread1).Start();
+            new Thread(Thread1).Start();
+            new Thread(Thread1).Start();
+
+            ManualResetEvent.Set();
+
+            Thread.Sleep(5000);
+        }
+
+        /// <summary> Отсутствие конкуренции </summary>
+        private static void Test2()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("\nThreads contest absence:");
+            ManualResetEvent.Set();
+
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(Thread1).Start();
+
+                // даем потоку фору
+                Thread.Sleep(10);
+            }
+
+            Thread.Sleep(5000);
+        }
+
+        private static void Main(string[] args)
+        {
+            Test1();
+            Test2();
+        }
+    }
+}
---
+++ HybridConstructions/SimpleHybridLock/SimpleHybridLock.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleHybridLock
+{
+    internal class SimpleHybridLock : IDisposable
+    {
+        private int _waitersCount = 0;
+
+        private readonly AutoResetEvent _autoResetEvent = new AutoResetEvent(false);
+
+        public void Enter()
+        {
+            var currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            Logger.Log(currentThreadId, "entered");
+            // используем конструкцию пользовательского режима для определения необходимости блокировки потока конструкцией ядра
+            var actualWaitersCount = Interlocked.Increment(ref _waitersCount);
+            if (actualWaitersCount == 1)
+            {
+                // поток получает право выполняться дальше
+                Logger.Log(currentThreadId, "got a green ligth");
+                return;
+            }
+
+            // блокируем поток конструкцией ядра
+            Logger.Log(currentThreadId, "got a red ligth and will use kernel costruction");
+            _autoResetEvent.WaitOne();
+            Logger.Log(currentThreadId, "got a green ligth after waiting");
+        }
+
+        public void Leave()
+        {
+            var currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            Logger.Log(currentThreadId, "is leaving");
+            // уменьшаем количество ожидающих потоков 
+            // (при правильном использовании гибридной конструкции прибегать к атомарному декременту необязательно)
+            _waitersCount--;
+            if (_waitersCount == 0)
+            {
+                // если ожидающих потоков нет, то конструкция ядра не использовалась и мы просто возвращаем управление
+                Logger.Log(currentThreadId, "has left without using kernel construction");
+                return;
+            }
+
+            // используя конструкция режима ядра, даем продолжить работу одному другому потоку
+            _autoResetEvent.Set();
+            Logger.Log(currentThreadId, "has left using kernel construction");
+        }
+        
+        public void Dispose()
+        {
+            _autoResetEvent.Dispose();
+        }
+    }
+}
