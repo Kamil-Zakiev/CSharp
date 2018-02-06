14c5164aa7a4c23087322974549d6cc4537b1144
Камиль Закиев <k.zakiev@bars-open.ru>
2018-02-06 17:06:27 +0300

    добавил проект asp.net mvc 5

---
+++ MyWebApp/MyWebApp/Global.asax.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Http;
+
+namespace MyWebApp
+{
+    public class Global : HttpApplication
+    {
+        private readonly Guid _appGuid;
+
+        protected Global()
+        {
+            _appGuid = Guid.NewGuid();
+            Log("Global ctor");
+            Thread.Sleep(5000);
+        }
+
+        private void Log(string methodName)
+        {
+            Console.WriteLine($"AppGuid: {_appGuid}: {methodName} was called by Thread #{Thread.CurrentThread.ManagedThreadId}." +
+                              $" Hashes: [HttpContext - {(object)Context?.GetHashCode() ?? "контекст пуст"}");
+        }
+
+        ~Global()
+        {
+            Log("Global finilize method");
+        }
+        
+        protected void Application_Start()
+        {
+            Log("Application_Start");
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
