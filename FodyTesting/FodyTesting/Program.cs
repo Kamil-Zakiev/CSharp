58ebc5b48f493f78e08bdecbdd756ee6267bc507
Камиль Закиев <k.zakiev@bars-open.ru>
2018-02-19 18:44:28 +0300

    nullGuard.Fody example

---
+++ FodyTesting/FodyTesting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using NullGuard;
+
+namespace FodyTesting
+{
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person();
+
+            try
+            {
+                person.Name = null;
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine();
+            }            
+            
+            var nameProp = person.GetType().GetTypeInfo().DeclaredProperties.Single(p => p.Name == "Name");
+            nameProp.SetValue(person, "123");
+            
+            Console.WriteLine(person.Name);
+        }
+    }
+}
