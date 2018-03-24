8ec67932236f41ee7d14a3c45167e614ca10ccdf
Камиль Закиев <k.zakiev@bars-open.ru>
2018-03-24 11:58:59 +0300

    implemented insert sort, began merge sort

---
+++ SortingAlgorithms/InsertSort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Helpers;
+
+namespace InsertSort
+{
+    public class InsertSort
+    {
+        public static void  Start(int[] a)
+        {
+            for (var i = 1; i < a.Length; i++)
+            {
+                var j = i;
+                while (j > 0 && a[j] < a[j-1])
+                {
+                    var z = a[j];
+                    a[j] = a[j - 1];
+                    a[j - 1] = z;
+                    j--;
+                }
+            }
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int count = 100000;
+            var array = Enumerable.Range(1, count).Shuffle().ToArray();
+
+            var sw = new Stopwatch();
+            sw.Start();
+            InsertSort.Start(array);
+            sw.Stop();
+
+            Console.WriteLine(sw.ElapsedMilliseconds + "ms");
+            // 4213ms for 100000
+
+            Console.WriteLine(array.IsAscSort());
+        }
+    }
+}
---
+++ SortingAlgorithms/MergeSort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Helpers;
+
+namespace MergeSort
+{
+    public class MergeSort
+    {
+        public static void StartInner(int[] a, int start = 0, int end = -1)
+        {
+            if (end == -1)
+            {
+                end = a.Length - 1;
+            }
+            
+            if()
+            
+            var delimiter = (start + end) / 2;
+            
+            
+            
+            
+        }
+
+        public static void Start(int[] a)
+        {
+            
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //const int count = 100000;
+            const int count = 10;
+            var array = Enumerable.Range(1, count).Shuffle().ToArray();
+
+            var sw = new Stopwatch();
+            sw.Start();
+            MergeSort.Start(array);
+            sw.Stop();
+
+            Console.WriteLine(sw.ElapsedMilliseconds + "ms");
+            // 17860ms for 100000
+
+            Console.WriteLine(array.IsAscSort());
+        }
+    }
+}
