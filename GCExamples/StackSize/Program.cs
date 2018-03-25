01ab8da5c77601a3f9b0927a6e2fb922f8c58440
Камиль Закиев <k.zakiev@mail.ru>
2018-03-25 13:14:18 +0300

    GC: stack size is not considered

---
+++ GCExamples/StackSize/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace StackSize
+{
+    class MyStruct
+    
+    {
+        public int Count;
+        public int Garbage1;
+        public int Garbage2;
+        public int Garbage3;
+        
+    }
+    internal class Program
+    {
+        private static void M(MyStruct myStruct)
+        {
+            if (myStruct.Count > 1000)
+            {
+                return;
+            }
+
+            myStruct.Count++;
+            M(myStruct);
+        }
+        
+        /// <summary>
+        /// GC.GetTotalMemory gets only heap, not a stack size
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            var startBytes = GC.GetTotalMemory(true);
+
+            var s = new MyStruct(); 
+            M(s);
+            
+            var endBytes = GC.GetTotalMemory(false);
+
+            Console.WriteLine(s.Count);
+            Console.WriteLine(endBytes - startBytes);
+        }
+    }
+}
