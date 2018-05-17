a66356922f11c03782008186408c19da1d952d86
Камиль Закиев <k.zakiev@bars-open.ru>
2018-05-17 19:20:26 +0300

    ctor of attribute is executed in runtime

---
+++ Attributes/CheckCtorExecutionTime/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CheckCtorExecutionTime
+{
+    using System;
+    using System.Reflection;
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class MyAttr : Attribute
+    {
+        public string SomeInfo { get; }
+        
+        public MyAttr(string someInfo)
+        {
+            SomeInfo = someInfo;
+            Console.WriteLine("ctor was executed");
+        }
+    }
+    
+    [MyAttr("test information")]
+    internal class Program
+    {
+        /// <summary> ctor of attribute is executed in runtime </summary>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+            var attr = typeof(Program).GetCustomAttribute<MyAttr>();
+            Console.WriteLine(2);
+            Console.WriteLine(attr.SomeInfo);
+            Console.WriteLine(3);
+        }
+    }
+}
