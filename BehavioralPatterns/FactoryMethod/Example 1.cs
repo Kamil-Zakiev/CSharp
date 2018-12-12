34632be882d5b04cadbe8691f82a5a517a87a748
Камиль Закиев <k.zakiev@mail.ru>
2018-12-12 21:40:18 +0300

    factory method examples

---
+++ BehavioralPatterns/FactoryMethod/Example 1.cs
@@ -0,0 +1,36 @@
+﻿namespace FactoryMethod
+{
+    public interface IProduct
+    {
+        int Price { get; }
+    }
+
+    public abstract class BaseClass
+    {
+        protected abstract IProduct CreateProduct();
+
+        public int CalculatePrice()
+        {
+            var product = CreateProduct();
+            return product.Price;
+        }
+    }
+
+    public class ConcreteProduct : IProduct
+    {
+        public ConcreteProduct(int price)
+        {
+            Price = price;
+        }
+
+        public int Price { get; }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        protected override IProduct CreateProduct()
+        {
+            return new ConcreteProduct(100);
+        }
+    }
+}
---
+++ BehavioralPatterns/FactoryMethod/Example 2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    public class SomeClass
+    {
+        private Func<IProduct> _priceFactory;
+
+        public SomeClass(Func<IProduct> priceFactory)
+        {
+            _priceFactory = priceFactory;
+        }
+
+        public int CalculatePrice()
+        {
+            var product = _priceFactory();
+            return product.Price;
+        }
+    }
+}
