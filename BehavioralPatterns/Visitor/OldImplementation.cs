cb7523e9a7ba589b059bfd89bf37502993b7dc34
Kamil Zakiev <v-kazak@microsoft.com>
2019-08-19 10:47:18 +0300

    visitor: added old implemantation example

---
+++ BehavioralPatterns/Visitor/OldImplementation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Visitor.OldImplementation
+{
+    public class CityPark
+    {
+        // some fields and methods ...
+    }
+
+    public class CityBuilding
+    {
+        // some fields and methods ...
+    }
+}
