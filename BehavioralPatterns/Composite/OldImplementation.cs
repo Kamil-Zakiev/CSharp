3136da360637ede9cb175c490988656f48484cda
Kamil Zakiev <v-kazak@microsoft.com>
2019-08-20 11:21:52 +0300

    added old implementation for composite

---
+++ BehavioralPatterns/Composite/OldImplementation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Composite.OldImplementation
+{
+    public class Box
+    {
+        private List<object> _items = new List<object>();
+
+        public IReadOnlyList<object> Items => _items;
+    }
+
+    public class Item
+    {
+        public int Price => 123;
+    }
+
+    public class SomeClass
+    {
+        private int CalculateBoxPrice(Box box)
+        {
+            return box.Items
+                .Sum(x =>
+                {
+                    switch (x)
+                    {
+                        case Box b: return CalculateBoxPrice(b);
+                        case Item i: return i.Price;
+                        default: return 0;
+                    }
+                });
+        }
+
+        public string GetOrderCost(Box box)
+        {
+            return "Order cost is " + CalculateBoxPrice(box);
+        }
+    }
+}
