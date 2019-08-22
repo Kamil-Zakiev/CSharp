b98bfc904f0b7240bdf0fa36f55d15810a42c2ed
Kamil Zakiev <zakiev-kamil@mai.ru>
2019-08-22 23:09:47 +0300

    added MethodInjection example

---
+++ BehavioralPatterns/DI/MethodInjection.cs
@@ -0,0 +1,15 @@
+﻿namespace DI.MethodInjection
+{
+    public interface ISomeContext
+    {
+
+    }
+
+    public class SomeClass
+    {
+        public void DoStuff(ISomeContext context)
+        {
+            // do something based on context
+        }
+    }
+}
