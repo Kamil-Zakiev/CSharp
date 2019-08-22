c90b31ac395866380b5d3a0d55a732d73bee3736
Kamil Zakiev <v-kazak@microsoft.com>
2019-08-22 11:48:48 +0300

    Added PropertyInjection example

---
+++ BehavioralPatterns/DI/PropertyInjection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DI.PropertyInjection
+{
+    public interface IDependency
+    {
+
+    }
+
+    internal class LocalDefault : IDependency
+    {
+
+    }
+
+    public class SomeClass
+    {
+        private IDependency _dependency;
+        public IDependency Dependency
+        {
+            get
+            {
+                return _dependency ?? new LocalDefault();
+            }
+            set
+            {
+                if(_dependency != null)
+                {
+                    throw new InvalidOperationException("Dependency is already created");
+                }
+
+                _dependency = value ?? throw new ArgumentNullException();
+            }
+        }
+    }
+}
