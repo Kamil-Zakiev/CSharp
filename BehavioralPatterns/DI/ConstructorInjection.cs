e1b2991079c17f753bce82b79f553b6013130d16
Kamil Zakiev <v-kazak@microsoft.com>
2019-08-22 11:21:09 +0300

    added ConstructorInjection example

---
+++ BehavioralPatterns/DI/ConstructorInjection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DI.ConstructorInjection
+{
+    public interface IDependency
+    {
+    }
+
+    public class ConcreteImplementationOfDependency : IDependency
+    {
+    }
+
+    public class SomeService
+    {
+        private readonly IDependency _dependency;
+
+        public SomeService(IDependency dependency)
+        {
+            _dependency = dependency ?? throw new ArgumentNullException(nameof(dependency));
+        }
+    }
+}
