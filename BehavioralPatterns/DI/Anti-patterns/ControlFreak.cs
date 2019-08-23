7f147247fd9da1ac28ce6dd05ac800893b17b646
Kamil Zakiev <v-kazak@microsoft.com>
2019-08-23 11:26:22 +0300

    added ControlFreak anti-pattern example

---
+++ BehavioralPatterns/DI/Anti-patterns/ControlFreak.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI.Anti_patterns.ControlFreak
+{
+    interface IDependency
+    {
+    }
+
+    class ConcreteImplementation : IDependency
+    {
+    }
+
+    class SomeClass
+    {
+        private readonly IDependency _dependency;
+
+        public SomeClass()
+        {
+            _dependency = new ConcreteImplementation();
+        }
+    }
+}
