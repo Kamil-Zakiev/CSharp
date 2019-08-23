20fdbcedcdfbcbde91ac612d5744f9ae390e1f8e
Kamil Zakiev <v-kazak@microsoft.com>
2019-08-23 10:41:34 +0300

    added AmbientContext example

---
+++ BehavioralPatterns/DI/AmbientContext.cs
@@ -0,0 +1,34 @@
+﻿namespace DI.AmbientContext
+{
+    public abstract class Context
+    {
+        private static Context _default = new DefaultContext();
+        private static Context _current;
+
+        public static Context Current
+        {
+            get
+            {
+                return _current ?? _default;
+            }
+            set
+            {
+                _current = value;
+            }
+        }
+    }
+
+    public class DefaultContext : Context
+    {        
+    }
+
+    public class SomeClass
+    {
+        public void Method()
+        {
+            var context = Context.Current;
+
+            // do something
+        }
+    }
+}
