5c7ea1d61ac07d7f1cc6d4b7d9392b42791ca265
Kamil Zakiev <zakiev-kamil@mai.ru>
2019-08-24 13:41:42 +0300

    added ServiceLocator example

---
+++ BehavioralPatterns/DI/Anti-patterns/ServiceLocator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DI.Anti_patterns.ServiceLocator
+{
+    interface IDependency
+    {
+    }
+
+    class Locator
+    {
+        public static T Resolve<T>() => throw new NotImplementedException();
+    }
+
+    class SomeClass
+    {
+        void Method()
+        {
+            var service = Locator.Resolve<IDependency>();
+
+            // do smth with service
+        }
+    }
+}
