59b5eec55b499bdc731cfb54805d1afa0b2f19d1
Kamil Zakiev <zakiev-kamil@mai.ru>
2019-08-24 12:57:37 +0300

    added ConstrainedConstruction example

---
+++ BehavioralPatterns/DI/Anti-patterns/ConstrainedConstruction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI.Anti_patterns.ConstrainedConstruction
+{
+    interface IService
+    {
+    }
+
+    class ConcreteService : IService
+    {
+        public ConcreteService()
+        {
+            // initialize
+        }
+    }
+
+    class SomeClass
+    {
+        void SomeMethod(Type serviceImplementationType)
+        {
+            var service = (IService)Activator.CreateInstance(serviceImplementationType);
+            // do something with service
+        }
+    }
+}
