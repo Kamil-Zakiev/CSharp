872a8575d4be9adb09aa3e77716d3c4bd134b5fd
Kamil Zakiev <v-kazak@microsoft.com>
2019-08-26 11:46:02 +0300

    added a solution example to ShortLivedDependencies

---
+++ BehavioralPatterns/DI/Examples/ShortLivedDependencies/Solution.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace DI.Examples.ShortLivedDependencies.Solution
+{
+    interface IService
+    {
+        void DoSomething();
+    }
+
+    interface IManagedService : IService, IDisposable
+    {
+    }
+
+    interface IManagedServiceFactory
+    {
+        IManagedService Create();
+    }
+
+    class ManagedService : IManagedService
+    {
+        public void Dispose()
+        {
+            CloseConnection();
+        }
+
+        public void DoSomething()
+        {
+            // opens a connection to DB
+        }
+
+        private void CloseConnection()
+        {
+            // closes the connection
+        }
+    }
+
+    class DatabaseService : IService
+    {
+        private readonly IManagedServiceFactory _managedServiceFactory;
+
+        public DatabaseService(IManagedServiceFactory managedServiceFactory)
+        {
+            _managedServiceFactory = managedServiceFactory;
+        }
+
+        public void DoSomething()
+        {
+            using (var service = _managedServiceFactory.Create())
+            {
+                service.DoSomething();
+            }
+        }
+    }
+
+    class SomeClass
+    {
+        private readonly IService _service;
+
+        public SomeClass(IService service)
+        {
+            _service = service;
+        }
+
+        void SomeMethod()
+        {
+            _service.DoSomething();
+
+            // todo: how to deal with closing connection?
+        }
+    }
+}
