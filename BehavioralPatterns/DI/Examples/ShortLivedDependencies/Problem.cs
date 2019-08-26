7e21f885680cd5651969f87dd54dbd9ba3e6914b
Kamil Zakiev <v-kazak@microsoft.com>
2019-08-26 10:56:58 +0300

    added a Problem example with Short lived dependencies

---
+++ BehavioralPatterns/DI/Examples/ShortLivedDependencies/Problem.cs
@@ -0,0 +1,37 @@
+﻿namespace DI.Examples.ShortLivedDependencies.Problem
+{
+    interface IService
+    {
+        void DoSomething();
+    }
+
+    class DatabaseService : IService
+    {
+        public void DoSomething()
+        {
+            // opens a connection to DB
+        }
+
+        public void CloseConnection()
+        {
+            // closes the connection
+        }
+    }
+
+    class SomeClass
+    {
+        private readonly IService _service;
+
+        public SomeClass(IService service)
+        {
+            _service = service;
+        }
+
+        void SomeMethod()
+        {
+            _service.DoSomething();
+
+            // todo: how to deal with closing connection?
+        }
+    }
+}
