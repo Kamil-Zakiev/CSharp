b525c827071f23f75fcc670e2e44479e99bf03b1
Kamil Zakiev <v-kazak@microsoft.com>
2019-08-27 10:29:05 +0300

    added  example of CyclicDependencies

---
+++ BehavioralPatterns/DI/Examples/CyclicDependencies/Problem.cs
@@ -0,0 +1,36 @@
+﻿namespace DI.Examples.CyclicDependencies.Problem
+{
+    interface IA
+    {
+    }
+
+    interface IB
+    {
+    }
+
+    class A : IA
+    {
+        public A(IB b)
+        {
+        }
+    }
+
+    class B : IB
+    {
+        public B(IA a)
+        {
+        }
+    }
+
+    class CompositionRoot
+    {
+        void Compose()
+        {
+            // how to create instances of A and B?
+            /*
+            var serviceA = new A(???);
+            var serviceB = new B(???);
+            */
+        }
+    }
+}
